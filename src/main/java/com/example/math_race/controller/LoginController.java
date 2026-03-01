b24fa9b67e21988e7324000cfe0dbe2387394151
Jake Walker <walker.jake.1299@gmail.com>
2026-03-01 20:32:31 +0200

    test http

--- src/main/java/com/example/math_race/controller/LoginController.java
+++ src/main/java/com/example/math_race/controller/LoginController.java
@@ -1,4 +1,14 @@
 package com.example.math_race.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+
+    // http://localhost:8080/test-login
+    @GetMapping("/test-login")
+    public String testLogin() {
+        return "השרת של Math Race עובד! הבקשה הגיעה אליי.";
+    }
 }
